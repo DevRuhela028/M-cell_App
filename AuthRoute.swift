0a4d03f74b94713df7336baec6c429dea6510f14
Dev Ruhela <144375524+DevRuhela028@users.noreply.github.com>
2025-04-29 09:23:23 +0530

    Create AuthRoute.swift

---
+++ AuthRoute.swift
@@ -0,0 +1,10 @@
+// AuthRoute.swift
+import Foundation
+
+enum AuthRoute: Hashable {
+    case login
+    case signup
+    case adminDashboard
+    case studentDashboard
+    case engineerDashboard
+}
